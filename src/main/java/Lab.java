3c9e19681109191de809d52176cfb6e625b72015
Alex Daughters <alexdaughters@gmail.com>
2023-08-02 15:44:52 +0000

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -3,6 +3,7 @@ public class Lab {
      * Your challenge is to implement a method to determine if an int is even or odd. The method should return
      * the String "Even" if the number is even, and "Odd" if it is odd.
      *
+     * 
      * There is a special operator in programming languages called modulus, which provides the remainder
      * after a division, represented by %.
      * For instance, 7%4=3, 7%3=1, 4%2=0, 5%2=1, etc.
